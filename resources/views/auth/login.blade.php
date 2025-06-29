ce8ba54d4387bbcfb435e511eb009ac9ecd9b253
agilalatas <agilalatas17@gmail.com>
2025-06-29 15:41:34 +0700

    benerin layout dan tampilan login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,46 +2,80 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+        <div class="flex-1">
+            <div class="text-center">
+                <div class="flex justify-center mx-auto">
+                    <h1 class="text-2xl lg:text-3xl font-bold">LOGIN</h1>
+                </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <p class="mt-3 text-gray-500">Masuk untuk mengakses akun Anda</p>
+            </div>
+
+            <div class="mt-8">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                            :value="old('email')" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <div class="flex justify-between mt-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox"
+                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                                name="remember">
+                            <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+
+                        @if (Route::has('password.request'))
+                            <a class="block underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                href="{{ route('password.request') }}">
+                                {{ __('Lupa kata sandi?') }}
+                            </a>
+                        @endif
+                    </div>
+
+
+                    <div class=" mt-8">
+                        <x-primary-button>
+                            {{ __('Log in') }}
+                        </x-primary-button>
+                    </div>
+
+                </form>
+
+                <p class="mt-6 text-sm text-center text-gray-400">Belum punya akun? <a href="{{ route('register') }}"
+                        class="text-blue-500 focus:outline-none focus:underline hover:underline">Daftar</a>.
+                </p>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <div class="hidden bg-cover lg:block lg:w-2/3"
+        style="background-image: url(https://images.unsplash.com/photo-1550677133-afc1a1608f23?q=80&w=1090&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
+        <div
+            class="flex items-center justify-center text-center  h-full px-20 bg-gray-900 bg-opacity-65 backdrop-blur-sm">
+            <div>
+                <h2 class="text-2xl font-almendra text-white sm:text-4xl">NutriPick</h2>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <p class="max-w-xl mt-3 text-gray-300 text-xl">
+                    Rekomendasi makanan sehat berdasarkan kebutuhan gizi
+                </p>
+            </div>
         </div>
+    </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,85 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+
+    <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+        <div class="flex-1">
+            <div class="text-center">
+                <div class="flex justify-center mx-auto">
+                    <h1 class="text-2xl lg:text-3xl font-bold">REGISTRASI</h1>
+                </div>
+
+                <p class="mt-3 text-gray-500">Daftarkan diri Anda!</p>
+            </div>
+
+            <div class="mt-8">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <!-- Name -->
+                    <div>
+                        <x-input-label for="name" :value="__('Nama')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                            :value="old('name')" required autofocus autocomplete="name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                            :value="old('email')" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-8">
+                        <x-primary-button>
+                            {{ __('Daftar') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+
+                <p class="mt-6 text-sm text-center text-gray-400">Sudah terdaftar? <a href="{{ route('login') }}"
+                        class="text-blue-500 focus:outline-none focus:underline hover:underline">Masuk</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="hidden bg-cover lg:block lg:w-2/3"
+        style="background-image: url(https://images.unsplash.com/photo-1550677133-afc1a1608f23?q=80&w=1090&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
+        <div
+            class="flex items-center justify-center text-center  h-full px-20 bg-gray-900 bg-opacity-65 backdrop-blur-sm">
+            <div>
+                <h2 class="text-2xl font-almendra text-white sm:text-4xl">NutriPick</h2>
+
+                <p class="max-w-xl mt-3 text-gray-300 text-xl">
+                    Rekomendasi makanan sehat berdasarkan kebutuhan gizi
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
@@ -48,5 +128,5 @@
                 {{ __('Register') }}
             </x-primary-button>
         </div>
-    </form>
+    </form> --}}
 </x-guest-layout>
