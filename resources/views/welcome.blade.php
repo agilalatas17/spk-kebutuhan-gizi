a139255d73eae1b5bfa36aa5002785745e8acf6d
agilalatas <agilalatas17@gmail.com>
2025-06-30 21:36:46 +0700

    membuat tampilan landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-    <h1>HAlaman hOme</h1>
+    <div class="w-full bg-cover bg-bottom bg-no-repeat h-screen"
+        style="background-image: url('{{ asset('assets/images/nutrition_large.jpg') }}');">
+        <div class="flex pt-28 lg:pt-40 justify-center w-full h-full">
+            <div class="text-center px-4 lg:w-1/2">
+                <h1 class="text-xl font-medium lg:text-4xl mb-2 lg:mb-4">Selamat datang di <span
+                        class="text-green-600 font-almendra">NutriPick</span>
+                </h1>
+                <p class="text-sm lg:text-lg text-pretty">Sistem rekomendasi makanan sehat berbasis kebutuhan gizi kamu!
+                    Dengan teknologi Simple
+                    Additive Weighting (SAW), kami bantu kamu menemukan pilihan makanan paling cocok, sehat, don
+                    seimbang hanya dalam hitungan detik. Cukup isi data kebutuhan gizimu, don sistem kami akan
+                    menyajikan rekomendasi terbaik yang bikin hidupmu lebih sehat, praktis, don pastinya lebih cerdas.
+                </p>
+
+            </div>
+        </div>
+    </div>
 </x-layout>
