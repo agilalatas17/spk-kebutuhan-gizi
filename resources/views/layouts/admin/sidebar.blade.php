45a4a22161fe81e3c77c9eb59dbcc996364b85ac
agilalatas <agilalatas17@gmail.com>
2025-07-04 18:53:31 +0700

    membuat UI halaman hasil

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -64,7 +64,7 @@
                 <span class="mx-4 font-medium">Penilaian</span>
             </x-side-nav-link>
 
-            <x-side-nav-link :href="url('#')" :active="request()->routeIs('#')">
+            <x-side-nav-link :href="url('/hasil')" :active="request()->routeIs('admin.hasil')">
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                     fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,10 @@ Route::get('/sub-kriteria', function () {
     return view('admin.sub-kriteria');
 })->middleware(['auth', 'verified'])->name('admin.sub-kriteria');
 
+Route::get('/hasil', function () {
+    return view('admin.hasil');
+})->middleware(['auth', 'verified'])->name('admin.hasil');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
