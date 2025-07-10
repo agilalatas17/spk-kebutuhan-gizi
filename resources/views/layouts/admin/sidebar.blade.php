f628978d51ef04756c0d4df97adea9f95be17649
agilalatas <agilalatas17@gmail.com>
2025-07-10 19:24:56 +0700

    fixing bug

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -3,7 +3,7 @@
         <img class="object-cover w-24 h-24 mx-2 rounded-full"
             src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
             alt="avatar">
-        <h4 class="mx-2 mt-2 font-bold text-gray-800">{{ Auth::user()->name }}</h4>
+        <h4 class="mx-2 mt-2 font-bold text-gray-800">{{ Str::ucfirst(Auth::user()->nama) }}</h4>
         <p class="mx-2 mt-1 text-sm font-medium text-gray-600">{{ Auth::user()->email }}</p>
     </div>
 
