cf743c3e3671850fca33995fffbd377fa3d397c9
agilalatas <agilalatas17@gmail.com>
2025-07-17 02:55:37 +0700

    fixed primary button component

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full px-4 py-2 uppercase font-semibold text-sm text-white tracking-wide transition-colors  duration-300 transform ease-in-out bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50 text-center']) }}>
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full px-4 py-2 uppercase font-semibold text-sm text-white tracking-wide transition-colors  duration-300 transform ease-in-out bg-violet-500 rounded-lg hover:bg-violet-400 focus:outline-none focus:bg-violet-300 focus:ring focus:ring-violet-300 focus:ring-opacity-50 text-center']) }}>
     {{ $slot }}
 </button>
