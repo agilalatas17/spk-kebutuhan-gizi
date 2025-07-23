8fb34464280e3f4f92f8e6f5ca3c1470b23b6601
agilalatas <agilalatas17@gmail.com>
2025-07-23 23:50:58 +0700

    update field makanan

--- database/migrations/2025_07_07_025046_create_makanan_table.php
+++ database/migrations/2025_07_07_025046_create_makanan_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
         Schema::create('tb_makanan', function (Blueprint $table) {
             $table->id();
             $table->string('nama_makanan', 50);
-            $table->text('deskripsi');
+            $table->enum('kalori', ['>450 kcal', '351-450 kcal', '251-351 kcal', '150-250 kcal', '<150 kcal']);
+            $table->enum('protein', ['<5 gr', '5-9 gr', '>10-14 gr', '15-20 gr', '>20 gr']);
+            $table->enum('lemak_sehat', ['<2 gr', '2-3 gr', '4-5 gr', '6-8 gr', '>8 gr']);
+            $table->enum('serat', ['<2 gr', '2-3 gr', '4-5 gr', '6-8 gr', '>8 gr']);
+            $table->enum('vitamin_mineral', ['<20%', '20-39%', '40-59%', '60-80%', '>80%']);
+            $table->enum('index_glikemik', [ '>80', '70-80', '56-69', '40-55', '40']);
+            $table->enum('usia', ['>70 tahun', '61-70 tahun', '46-60 tahun', '31-45 tahun', '19-30 tahun']);
             $table->timestamps();
         });
     }
